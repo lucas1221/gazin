34532bda777deb09d8fc0627ea6b5765f8ce0cf6
Lucas G. de Queiroz <51690058+lucas1221@users.noreply.github.com>
2022-03-20 04:36:45 -0400

    Add files via upload

--- frontend/GazinApp/services/Context.js
+++ frontend/GazinApp/services/Context.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { useReducer } from 'react/cjs/react.production.min';
 
 export const Context = React.createContext({});
 
@@ -32,6 +33,57 @@ export default function Provider({children}) {
     const [modalExcluirLevelVisible, setModalExcluirLevelVisible] = useState(false);
     // Para os Formularios
     const [textEditarLevel, setTextEditarLevel] = React.useState('');
+    const [setIdEditarDev] = React.useState({});
+    const [setIdExcluirDev] = React.useState({});
+    const [setIdEditarLevel] = React.useState([]);
+    const [setIdExcluirLevel] = React.useState([]);
+    const [data, setData] = React.useState([]);
+    const [nivel, setNivel] = React.useState([]);
+    /* Reducers Utilizados */
+    // Reducer para Editar Devs
+    function reducer(idEditarDev, action) {
+        if (action.type === 'update users')
+        {
+            setModalEditarDevVisible(true);
+            const editarDevID = action.payload;
+            return editarDevID
+        }
+        return idEditarDev;
+    }
+    // Reducer para Excluir Devs
+    function reducerExcluirDev(idExcluirDev, action) {
+        if (action.type === 'delete users')
+        {
+            setModalExcluirDevVisible(true);
+            const ExcluirDevID = action.payload;
+            return ExcluirDevID
+        }
+        return idExcluirDev;
+    }
+    // Reducer para Editar Niveis
+    function reducerLevel(idEditarLevel, action) {
+        if (action.type === 'update levels')
+        {
+            setModalEditarLevelVisible(true);
+            const editarNivelID = action.payload;
+            return editarNivelID
+        }
+        return idEditarLevel;
+    }
+    // Reducer para Excluir Niveis
+    function reducerExcluirLevel(idExcluirLevel, action) {
+        if (action.type === 'delete levels')
+        {
+            setModalExcluirLevelVisible(true);
+            const ExcluirLevelID = action.payload;
+            return ExcluirLevelID
+        }
+        return idExcluirLevel;
+    }
+    const [idEditarDev, dispatch] = React.useReducer(reducer, data);
+    const [idExcluirDev, dispatchExcluirDev] = React.useReducer(reducerExcluirDev, data);
+    const [idEditarLevel, dispatchEditarLevel] = React.useReducer(reducerLevel, nivel);
+    const [idExcluirLevel, dispatchExcluirLevel] = React.useReducer(reducerExcluirLevel, nivel);
 
     return( 
             <Context.Provider 
@@ -39,7 +91,8 @@ export default function Provider({children}) {
             setTextDataNascimento, textHobby, setTextHobby, textSexo, setTextSexo, modalEditarDevVisible, setModalEditarDevVisible, modalExcluirDevVisible, setModalExcluirDevVisible, 
             textEditarNome,setTextEditarNome, textEditarNivel, setTextEditarNivel, textEditarIdade, setTextEditarIdade, textEditarDataNascimento, setTextEditarDataNascimento, 
             textEditarHobby, setTextEditarHobby, textEditarSexo, setTextEditarSexo, modalNiveisVisible, setModalCadastrarNivelVisible, textLevel, setTextLevel, modalEditarLevelVisible, 
-            setModalEditarLevelVisible, modalExcluirLevelVisible, setModalExcluirLevelVisible, textEditarLevel, setTextEditarLevel}}>
+            setModalEditarLevelVisible, modalExcluirLevelVisible, setModalExcluirLevelVisible, textEditarLevel, setTextEditarLevel, idEditarDev, setIdEditarDev, idExcluirDev, setIdExcluirDev,
+            idEditarLevel, setIdEditarLevel, idExcluirLevel, setIdExcluirLevel, data, setData, nivel, setNivel, dispatch, dispatchExcluirDev, dispatchEditarLevel, dispatchExcluirLevel}}>
             {children}
             </Context.Provider>
     );
--- frontend/GazinApp/services/api.js
+++ frontend/GazinApp/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instancia = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://192.168.100.12:8080/',
 })
 
 export default instancia;
